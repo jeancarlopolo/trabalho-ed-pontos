ab19a6ce6f0ccd1589a6831bd076e1311b9e3344
tonhão mototaxi <jean.carlo.polo@uel.br>
2022-11-10 10:35:47 -0300

    searchcpt eu acho

--- jeancp/src/cpt.c
+++ jeancp/src/cpt.c
@@ -34,14 +34,6 @@ CPTree createCPT(double epsilon)
 
 bool insertCPT(CPTree b, double x, double y, Info info, VisitaNo vfindo, VisitaNo vfvoltando, void *extra)
 {
-    /**
-     * Se a arvore for vazia, cria um novo no e o insere na raiz
-     * Se nao for vazia, chama a funcao recursiva
-     * Mas a função recebe uma arvore e nao um no?
-     * Como a funcao recursiva vai saber onde ela está?
-     * Como a funcao recursiva vai saber se o no ja existe?
-     * Como a funcao recursiva vai saber o epsilon?
-     */
     struct arvore *a = b;
     struct node *atual = b;
     bool inseriu;
@@ -143,4 +135,84 @@ bool insertCPT(CPTree b, double x, double y, Info info, VisitaNo vfindo, VisitaN
     }
 }
 
-// searchCPT, removeCPT, findInRegionCPT, getInfoCPT, getCircCPT, percursoSimetrico, dfs, bfs
+bool searchCPT(CPTree b, double x, double y, Info *info, VisitaNo vfindo, VisitaNo vfvoltando, void *extra)
+{
+    struct arvore *a = b;
+    struct node *atual = b;
+    bool achou;
+    // b é um elemento
+    if (a->isArvore != true)
+    {
+        // Se achou o elemento, retorna true e seta o info
+        if (fabs(atual->x - x) < atual->epsilon && fabs(atual->y - y) < atual->epsilon)
+        {
+            atual->info = info;
+            return true;
+        }
+        else
+        {
+            // se for menor vai pra esquerda
+            if (atual->x > x || (atual->x == x && atual->y > y))
+            {
+                // se o no da esquerda for vazio retorna falso
+                if (atual->esq == NULL)
+                {
+                    return false;
+                }
+                // se nao for vazio chama a funcao recursiva
+                else
+                {
+                    if (vfindo != NULL)
+                    {
+                        vfindo(atual->x, atual->y, atual->info, atual->circ.x, atual->circ.y, atual->circ.raio, extra);
+                    }
+                    achou = searchCPT(atual->esq, x, y, info, vfindo, vfvoltando, extra);
+                    if (vfvoltando != NULL)
+                    {
+                        vfvoltando(atual->x, atual->y, atual->info, atual->circ.x, atual->circ.y, atual->circ.raio, extra);
+                    }
+                    return achou;
+                }
+            }
+            // se for maior vai pra direita
+            else
+            {
+                // se o no da direita for vazio, cria um novo no e insere
+                if (atual->dir == NULL)
+                {
+                    return false;
+                }
+                // se nao for vazio chama a funcao recursiva
+                else
+                {
+                    if (vfindo != NULL)
+                    {
+                        vfindo(atual->x, atual->y, atual->info, atual->circ.x, atual->circ.y, atual->circ.raio, extra);
+                    }
+                    achou = searchCPT(atual->dir, x, y, info, vfindo, vfvoltando, extra);
+                    if (vfvoltando != NULL)
+                    {
+                        vfvoltando(atual->x, atual->y, atual->info, atual->circ.x, atual->circ.y, atual->circ.raio, extra);
+                    }
+                    return achou;
+                }
+            }
+        }
+    }
+    // b é uma arvore
+    else
+    {
+        // se a arvore for vazia, retorna falso
+        if (a->raiz == NULL)
+        {
+            return false;
+        }
+        // se nao for vazia chama a funcao recursiva na raiz
+        else
+        {
+            return searchCPT(a->raiz, x, y, info, vfindo, vfvoltando, extra);
+        }
+    }
+}
+
+// removeCPT, findInRegionCPT, getInfoCPT, getCircCPT, percursoSimetrico, dfs, bfs
